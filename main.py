ddccdc0175498fba422634e68ef0e67943014b90
kwantova <kwantova@gmail.com>
2024-10-19 11:29:41 +0300

    little bug fixed

--- main.py
+++ main.py
@@ -11,34 +11,37 @@ while True:
         break
     if num == -1: break
     nums.append(num)
+try:
+    print("Математическое ожидание введенной Вами выборки:", a.custom_mean(nums))
+    print("Дисперсия введенной Вами выборки:", a.custom_var(nums))
 
-print("Математическое ожидание введенной Вами выборки:", a.custom_mean(nums))
-print("Дисперсия введенной Вами выборки:", a.custom_var(nums))
+    print("-----  Данные для диаграммы Тьюки -----") -u
 
-print("-----  Данные для диаграммы Тьюки -----")
+    lq = t.split_sample(nums, p = 25)
+    print("Нижний квартиль:", lq)
+    median = t.split_sample(nums, p = 50)
+    print("Медиана:", median)
+    uq = t.split_sample(nums, p = 75)
+    print("Верхний квартиль:", uq)
+    uw = a.custom_max(nums)
+    print("Верхний ус:", uw)
+    lw = a.custom_min(nums)
+    print("Нижний ус:", lw)
 
-lq = t.split_sample(nums, p = 25)
-print("Нижний квартиль:", lq)
-median = t.split_sample(nums, p = 50)
-print("Медиана:", median)
-uq = t.split_sample(nums, p = 75)
-print("Верхний квартиль:", uq)
-uw = a.custom_max(nums)
-print("Верхний ус:", uw)
-lw = a.custom_min(nums)
-print("Нижний ус:", lw)
+    ch = input("Отобразить диаграмму Тьюки графически? [y/n]: ")
+    if ch == "y" or ch == "Y":
+        cleannums = t.anomal_check(nums)
+        lqc = t.split_sample(cleannums, p = 25)
+        medianc = t.split_sample(cleannums, p = 50)
+        uqc = t.split_sample(cleannums, p = 75)
+        uwc = a.custom_max(cleannums)
+        lwc = a.custom_min(cleannums)
+        g.custom_tukey(lwc, lqc, medianc, uqc, uwc)
 
-ch = input("Отобразить диаграмму Тьюки графически? [y/n]: ")
-if ch == "y" or ch == "Y":
-    cleannums = t.anomal_check(nums)
-    lqc = t.split_sample(cleannums, p = 25)
-    medianc = t.split_sample(cleannums, p = 50)
-    uqc = t.split_sample(cleannums, p = 75)
-    uwc = a.custom_max(cleannums)
-    lwc = a.custom_min(cleannums)
-    g.custom_tukey(lwc, lqc, medianc, uqc, uwc)
+    elif ch == "n" or ch == "N":
+        print("Сеанс завершен.")
+    else:
+        print ("Команда не распознана.")
 
-elif ch == "n" or ch == "N":
-    print("Сеанс завершен.")
-else:
-    print ("Команда не распознана.")
+except Exception:
+    print("При вводе данных произошла ошибка. Перезапустите программу.")
